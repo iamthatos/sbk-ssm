0c1e275b969d7a17cb52d683d78a84af5bba4354
thato <thato.seboko@gmail.com>
2020-05-15 14:01:03 +0200

    init-dependencies

---
+++ src/main/java/co/za/sbk/sbkssm/SbkSsmApplication.java
@@ -0,0 +1,13 @@
+package co.za.sbk.sbkssm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbkSsmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbkSsmApplication.class, args);
+    }
+
+}
